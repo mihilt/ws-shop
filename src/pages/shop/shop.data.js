20cbbc28094474428a270512ef587abb516efac3
mihilt <50368758+mihilt@users.noreply.github.com>
2020-02-21 13:36:14 +0900

    shop state update

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,78 @@
+const SHOP_DATA = [
+    {
+        id: 1,
+        title: '의류',
+        routeName: 'cloth',
+        items: [
+            {
+                id: 1,
+                name: '무스탕',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_114949-1.jpg?w=1100',
+                price: 59800
+            },
+            {
+                id: 2,
+                name: '깔깔이',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_115125.jpg?w=1100',
+                price: 39800
+            },
+            {
+                id: 3,
+                name: '땡땡이 모자',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_115440.jpg?w=1100',
+                price: 35
+            },
+            {
+                id: 4,
+                name: '흰 모자',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_115526.jpg?w=1100',
+                price: 25
+            },
+            {
+                id: 5,
+                name: '건빵 모자',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_115631.jpg?w=1100',
+                price: 18
+            },
+            {
+                id: 6,
+                name: '빨간 모자',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_120400.jpg?w=1100',
+                price: 14
+            },
+            {
+                id: 7,
+                name: '등산 모자',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_120423.jpg?w=1100',
+                price: 18
+            },
+            {
+                id: 8,
+                name: '전역모',
+                imageUrl: 'https://mihilthome.files.wordpress.com/2020/02/20200221_121821.jpg?w=1100',
+                price: 14
+            }
+        ]
+    },
+    {
+      id: 2,
+      title: '전자기기',
+      routeName: 'electronics',
+      items: [
+        {
+          id: 1,
+          name: '폰',
+          imageUrl: '',
+          price: 123123
+        },
+        {
+          id: 2,
+          name: '폰2',
+          imageUrl: '',
+          price: 123123
+        }
+      ]
+    }
+]
+
+export default SHOP_DATA;
